7a91417f89e7f186c12d65464042ca8e011210e0
Taha <taha.mahmoud.abdellah@gmail.com>
2025-04-09 21:33:08 +0200

    update simple CRUD method

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,16 @@
 <?php
-
+namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+/*use app\Http\Controllers\PostController;*/
 
 //Route::get('/user', function (Request $request) {
 //    return $request->user();
 //})->middleware('auth:sanctum');
 
-Route::get("/",function (){
-    return 'API';
-});
+//Route::get("/",function (){
+//    return 'API';
+//});
+
+
+Route::apiResource('posts',PostController::class);
